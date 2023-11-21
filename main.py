dc40aa261915805650ff01c0964eabba499459cf
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-21 20:23:17 +0000

    commited

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import functions from necessary modules
 from random import randint
 from math import trunc
 from time import sleep
@@ -6,29 +7,30 @@ import os
 
 generate_row = []
 
-
-grid_size = int(input())
+grid_size = int(input("how big grid: "))
 
 final_grid = []
+# create grid with only black squares
 grid = [[ "⬛" for i in range(grid_size)] for j in range(grid_size)]
 
-def display_grid():
+def display_grid(): # displays the final grid
     for i in final_grid:
         print("".join(i))
 
 def run_conway(grid, final_grid):
     while True:
+        os.system('cls')
         display_grid()
 
-        for row_idx, row in enumerate(grid):
-            for idx, cell in enumerate(row):
+        for row_idx, row in enumerate(grid): # for every row in the grid
+            for idx, cell in enumerate(row): # for every cell in the row
                 alive_neighbors = 0
 
-                for i in range(-1, 2):
+                for i in range(-1, 2): # both for loops are for deciding which surrounding value to check
                     for j in range(-1, 2):
                         if i == 0 and j == 0:
                             continue
-
+                        
                         neighbor_row = row_idx + i
                         neighbor_col = idx + j
                         try:                    
@@ -50,8 +52,6 @@ def run_conway(grid, final_grid):
 display_grid()
 
 choice = input(f"write r to random gen a pattern (will add custom in future)").lower().strip()
-
-
 
 if choice == "r":
     for i in range(randint(trunc((grid_size * grid_size) / 8), trunc((grid_size * grid_size) / 4))):
