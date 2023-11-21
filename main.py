87ec12e782ff2d6139a16f838ad4e943adfdc5b3
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-21 20:01:50 +0000

    holy it finally works fianllyl

--- main.py
+++ main.py
@@ -1,84 +1,55 @@
 from random import randint
 from math import trunc
 from time import sleep
+from copy import deepcopy
 import os
 
-
-
 generate_row = []
-
-what_to_do = {
-    1: [-1, -1],
-    2: [-1, 0],
-    3: [-1, 1],
-    4: [-1, 0],
-    5: [1, 0],
-    6: [-1, -1],
-    7: [-1, 0],
-    8: [-1, 1],
-}
 
 
 grid_size = int(input())
 
-
+final_grid = []
 grid = [[ "⬛" for i in range(grid_size)] for j in range(grid_size)]
-final_grid = grid
 
 def display_grid():
     for i in final_grid:
         print("".join(i))
 
-def run_conway():
+def run_conway(grid, final_grid):
     while True:
-        os.system("cls")
-        for row_idx, row in enumerate(grid):
-            for idx, i in enumerate(row):
-                if i == "⬜":
-                    current_surronding = 1
-                    dead_amount = 0
-                    alive_amount = 0
-                    while True:
-                        if current_surronding == 9:
-                            break
-                        list_to_check = what_to_do[current_surronding]
-                        try:
-                            colour = grid[row_idx + list_to_check[0]][idx + list_to_check[1]]
-                            if colour == "⬛":
-                                dead_amount += 1
-                            else:
-                                alive_amount += 1
-                        except IndexError:
-                            pass
-                        current_surronding += 1
-                    if dead_amount < 2 or alive_amount > 3:
-                        final_grid[row_idx][idx] = "⬛"
-                elif i == "⬛":
-                    current_surronding = 1
-                    dead_amount = 0
-                    alive_amount = 0
-                    while True:
-                        if current_surronding == 9:
-                            break
-                        list_to_check = what_to_do[current_surronding]
-                        try:
-                            colour = grid[row_idx + list_to_check[0]][idx + list_to_check[1]]
-                            if colour == "⬛":
-                                dead_amount += 1
-                            else:
-                                alive_amount += 1
-                        except IndexError:
-                            pass
-                        current_surronding += 1
-                    if alive_amount == 3:
-                        final_grid[row_idx][idx] = "⬜"
         display_grid()
-        sleep(0.5)
+
+        for row_idx, row in enumerate(grid):
+            for idx, cell in enumerate(row):
+                alive_neighbors = 0
+
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if i == 0 and j == 0:
+                            continue
+
+                        neighbor_row = row_idx + i
+                        neighbor_col = idx + j
+                        try:                    
+                            if grid[neighbor_row][neighbor_col] == "⬜":
+                                alive_neighbors += 1
+                        except IndexError:
+                            pass
+                if cell == "⬜" and (alive_neighbors < 2 or alive_neighbors > 3):
+                    final_grid[row_idx][idx] = "⬛"
+                elif cell == "⬜" and (alive_neighbors == 2 or alive_neighbors == 3):
+                    cell = "⬜"
+                elif cell == "⬛" and alive_neighbors == 3:
+                    final_grid[row_idx][idx] = "⬜"
+
+        grid = deepcopy(final_grid)
+        pc = input()
 
 
 display_grid()
 
-choice = input(f"would you like to randomly generate a pattern, load a pattern, or create a pattern? (r/l/c)").lower().strip()
+choice = input(f"write r to random gen a pattern (will add custom in future)").lower().strip()
 
 
 
@@ -87,9 +58,9 @@ if choice == "r":
         row = randint(1,grid_size)
         col = randint(1,grid_size)
         grid[row - 1][col - 1] = "⬜"
+else:
+    pass
 
-final_grid = grid
+final_grid = deepcopy(grid)
 
-run_conway()
-
-
+run_conway(grid, final_grid)
