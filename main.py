bb5c4e29e22b46fbaaeef84b7cfe26fa5b81f446
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-22 10:29:38 +0000

    editor framework

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 import os
 
 generate_row = []
-
+alphabet = [chr(i) for i in range(97, 123)]
 grid_size = int(input("how big grid: "))
 
 final_grid = []
@@ -28,25 +28,25 @@ def run_conway(grid, final_grid):
 
                 for i in range(-1, 2): # both for loops are for deciding which surrounding value to check
                     for j in range(-1, 2):
-                        if i == 0 and j == 0:
+                        if i == 0 and j == 0: # if its the centre value restarts the loop
                             continue
                         
-                        neighbor_row = row_idx + i
+                        neighbor_row = row_idx + i # current neighbour = current square + offset
                         neighbor_col = idx + j
                         try:                    
-                            if grid[neighbor_row][neighbor_col] == "⬜":
-                                alive_neighbors += 1
-                        except IndexError:
+                            if grid[neighbor_row][neighbor_col] == "⬜": # if the neighbour square is white
+                                alive_neighbors += 1 # add one to alive neighbours
+                        except IndexError: # handles border errors 
                             pass
-                if cell == "⬜" and (alive_neighbors < 2 or alive_neighbors > 3):
-                    final_grid[row_idx][idx] = "⬛"
-                elif cell == "⬜" and (alive_neighbors == 2 or alive_neighbors == 3):
+                if cell == "⬜" and (alive_neighbors < 2 or alive_neighbors > 3): # if current cell is white and there are less than 2 or more than 3 neighbours
+                    final_grid[row_idx][idx] = "⬛" # current cell dies
+                elif cell == "⬜" and (alive_neighbors == 2 or alive_neighbors == 3): # this code might be unnecessary but im too scared to change it and break everything
                     cell = "⬜"
-                elif cell == "⬛" and alive_neighbors == 3:
+                elif cell == "⬛" and alive_neighbors == 3: # if a dead cell as 3 alive neighbours, it becomes alive
                     final_grid[row_idx][idx] = "⬜"
 
-        grid = deepcopy(final_grid)
-        pc = input()
+        grid = deepcopy(final_grid) # set grid to final grid
+        pc = input() # allows user to advance frame by frame
 
 
 display_grid()
@@ -58,9 +58,22 @@ if choice == "r":
         row = randint(1,grid_size)
         col = randint(1,grid_size)
         grid[row - 1][col - 1] = "⬜"
+    final_grid = deepcopy(grid)
+
+    run_conway(grid, final_grid)
+if choice == "e" and grid_size < 26:
+    new_row = []
+    for i in range(grid_size):
+        new_row.append(str(f'{i + 1}'))
+    grid.insert(0, new_row)
+    for idx, row in enumerate(grid):
+        if idx == 0:
+            continue
+        row.insert(0, alphabet[idx - 1])
+
+    final_grid = deepcopy(grid)
+    display_grid()
+
 else:
     pass
 
-final_grid = deepcopy(grid)
-
-run_conway(grid, final_grid)
