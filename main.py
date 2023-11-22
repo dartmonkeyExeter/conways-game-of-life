61a7fbe96c6850c87b7ee032146e6e395e4ee6b8
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-22 12:01:26 +0000

    functioning editor
    no save system but ill make one

--- main.py
+++ main.py
@@ -8,6 +8,34 @@ import os
 generate_row = []
 alphabet = [chr(i) for i in range(97, 123)]
 grid_size = int(input("how big grid: "))
+letter_to_number = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26
+}
 
 final_grid = []
 # create grid with only black squares
@@ -61,19 +89,42 @@ if choice == "r":
     final_grid = deepcopy(grid)
 
     run_conway(grid, final_grid)
-if choice == "e" and grid_size < 26:
-    new_row = []
+if choice == "e" and grid_size <= 26:
+    new_row = ["  "]
+    new_row_2 = ["  "]
     for i in range(grid_size):
-        new_row.append(str(f'{i + 1}'))
+        if i < 9:
+            new_row_2.append("_ ")
+            new_row.append(str(f'{i + 1} '))
+        else:
+            new_row_2.append(f'{str(i + 1)[0]} ')
+            new_row.append(f'{str(i + 1)[1]} ')
     grid.insert(0, new_row)
+    grid.insert(0, new_row_2)
     for idx, row in enumerate(grid):
-        if idx == 0:
+        if idx == 0 or idx == 1:
             continue
-        row.insert(0, alphabet[idx - 1])
+        row.insert(0, alphabet[idx - 2])
 
-    final_grid = deepcopy(grid)
-    display_grid()
-
+    while True:
+        final_grid = deepcopy(grid)
+        display_grid()
+        to_switch = input("Type save to stop editing.\nT the grid coordinate (letter first) here: ").lower().strip()
+        if to_switch == "save":
+            break
+        coord_char = to_switch[0]
+        try: coord_num = int(to_switch[1:])
+        except ValueError: 
+            print("Please enter a valid grid coordinate.") 
+            continue
+        if len(to_switch) < 2 or len(to_switch) > 3 or coord_char not in alphabet or coord_num > 26 or coord_num < 1:
+            print("Please enter a valid grid coordinate.")
+            continue
+        row = letter_to_number[coord_char] + 1
+        col = coord_num
+        if grid[row][col] == "⬛":
+            grid[row][col] = "⬜"
+        else: grid[row][col] = "⬛"
 else:
     pass
 
